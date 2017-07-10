8dadc66ac47cbe751b6f12a01b000a919a01aeec
Georg Brandl <georg@python.org>
2017-07-10 20:39:07 +0200

    Remove some unused support functions.

--- src/support.rs
+++ src/support.rs
@@ -89,116 +89,13 @@ pub enum ExitCode {
 }
 pub use self::ExitCode::*;
 
-pub trait Lengthable {
-    fn get_len(&self) -> isize;
-}
-pub fn length<A: Lengthable>(left: A) -> isize {
-    Lengthable::get_len(&left)
-}
-impl<T> Lengthable for Vec<T> {
-    fn get_len(&self) -> isize {
-        self.len() as isize
-    }
-}
-
-pub trait Bindable<T> {
-    fn bind_it(self, right: T) -> Self;
-}
-pub fn __op_bind<A: Bindable<B>, B>(left: A, b: B) -> A {
-    Bindable::bind_it(left, b)
-}
-impl<T: Display> Bindable<T> for String {
-    fn bind_it(mut self, right: T) -> Self {
-        // TODO
-        self.push_str(&format!("{}", right));
-        self
-    }
-}
-
-
 pub fn __op_forwardslash<A, B>(left: A, right: B) -> B {
     // TODO
     right
 }
 
-pub fn __op_dollarnot<A, B>(left: A, right: B) -> B {
-    // TODO
-    right
-}
-
-
-pub fn union<A: PartialEq>(mut left: Vec<A>, right: Vec<A>) -> Vec<A> {
-    for item in right {
-        if left.iter().position(|x| *x == item).is_none() {
-            left.push(item);
-        }
-    }
-    left
-}
-
-pub fn toInteger<T: Display>(left: T) -> isize {
-    // TODO
-    0
-}
-
-pub fn fromInteger(left: isize) -> String {
-    // TODO
-    "".to_string()
-}
-
-pub fn shiftL(l: isize, r: isize) -> isize {
-    l << r
-}
-
-pub fn shiftR(l: isize, r: isize) -> isize {
-    l >> r
-}
-
-pub fn fromEnum<A: ToPrimitive>(arg: A) -> isize {
-    arg.to_isize().unwrap()
-}
-
-pub fn __op_dotted_and(l: isize, r: isize) -> isize {
-    l & r
-}
-
-pub fn __op_dotted_or(l: isize, r: isize) -> isize {
-    l | r
-}
-
 pub fn __op_assign_div(l: isize, r: isize) -> isize {
     l / r
-}
-
-pub fn __op_tuple2<A, B>(left: A, right: B) -> (A, B) {
-    (left, right)
-}
-
-pub fn __op_power(l: isize, r: isize) -> isize {
-    //TODO
-    l
-}
-
-pub fn __mod(l: isize, r: isize) -> isize {
-    // TODO
-    l
-}
-
-pub fn not(left: bool) -> bool {
-    !left
-}
-
-pub fn __break<T: Clone, F: Fn(T) -> bool>(cond: F, input: Vec<T>) -> (Vec<T>, Vec<T>) {
-    let mut left = vec![];
-    let mut right = vec![];
-    for item in input.into_iter() {
-        if right.is_empty() && cond(item.clone()) {
-            left.push(item);
-        } else {
-            right.push(item);
-        }
-    }
-    (left, right)
 }
 
 pub fn __break_str<F: Fn(char) -> bool>(cond: F, input: String) -> (String, String) {
@@ -259,27 +156,12 @@ pub fn isDigit(input: char) -> bool {
     false
 }
 
-pub fn head(input: Vec<char>) -> char {
-    input[0]
-}
 
-pub fn head_str(input: String) -> char {
-    input.chars().nth(0).unwrap()
-}
-
-pub fn init(mut input: Vec<char>) -> Vec<char> {
-    input.pop();
-    input
-}
 
 pub fn init_str(input: String) -> String {
     let mut v: Vec<_> = input.chars().collect();
     v.pop();
     v.into_iter().collect()
-}
-
-pub fn tail(input: Vec<char>) -> Vec<char> {
-    input[1..].to_vec()
 }
 
 pub fn tail_str(input: String) -> String {
@@ -290,16 +172,8 @@ pub fn fst<A, B>(input: (A, B)) -> A {
     input.0
 }
 
-pub fn snd<A, B>(input: (A, B)) -> B {
-    input.1
-}
-
 pub fn flip<A, B, C, F: Fn(A, B) -> C>(input: F, b: B, a: A) -> C {
     input(a, b)
-}
-
-pub fn take(len: isize, input: Vec<String>) {
-    // TODO
 }
 
 pub fn take_str(len: isize, input: String) -> String {
@@ -321,19 +195,6 @@ pub fn addExtension(fp: FilePath, ext: &str) -> FilePath {
     fp
 }
 
-
-pub fn takeWhile<T: Clone, F: Fn(T) -> bool>(cond: F, input: Vec<T>) -> Vec<T> {
-    let mut left = vec![];
-    for item in input.into_iter() {
-        if cond(item.clone()) {
-            left.push(item);
-        } else {
-            return left
-        }
-    }
-    left
-}
-
 pub fn takeWhile_str<F: Fn(char) -> bool>(cond: F, input: String) -> String {
     let mut left = vec![];
     for item in input.chars() {
@@ -351,43 +212,12 @@ pub fn fromIntegral(left: isize) -> isize {
     left
 }
 
-pub fn drop<T>(len: isize, mut input: Vec<T>) -> Vec<T> {
-    for _ in 0..len {
-        input.remove(0);
-    }
-    input
-}
-
 pub fn drop_str(len: isize, input: String) -> String {
     input.chars().skip(len as usize).collect()
 }
 
-pub fn dropWhile<F: Fn(char) -> bool>(cond: F, input: String) -> String {
-    let mut out = vec![];
-    for item in input.chars() {
-        if cond(item.clone()) && out.is_empty() {
-            // skip
-        } else {
-            out.push(item);
-        }
-    }
-    out.into_iter().collect()
-}
-
 pub fn chr(input: isize) -> char {
     input as u8 as char
-}
-
-pub fn id<A>(input: A) -> A {
-    input
-}
-
-pub fn __boxed_chars(input: String) -> Box<[char]> {
-    input.chars().collect::<Vec<_>>().into_boxed_slice()
-}
-
-pub fn __boxed_slice<T: Sized>(input: Vec<T>) -> Box<[T]> {
-    input.into_boxed_slice()
 }
 
 // bits
@@ -403,50 +233,6 @@ pub fn clearBit(left: isize, right: isize) -> isize {
 pub fn testBit(left: isize, right: isize) -> bool {
     left & (1 << right) != 0
 }
-
-
-
-
-
-
-// Monads
-
-pub fn __return<A: Into<B>, B>(left: A) -> B {
-    left.into()
-}
-// pub trait Functor {
-//   fmap = liftM
-// }
-
-// pub trait Applicative P where
-//   pure = return
-//   (<*>) = ap
-
-// pub trait Monad<P> {
-//   fn ret(Self) -> Self;
-//   fn bind(Self) -> Self;
-//   fn fail(m) -> Self;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // ShowS, ReadS
@@ -552,15 +338,6 @@ macro_rules! __map {
 }
 
 #[macro_export]
-macro_rules! __fmap {
-    ($fn: expr, $target: expr) => {
-        $target.into_iter()
-            .map($fn)
-            .collect::<Vec<_>>()
-    }
-}
-
-#[macro_export]
 macro_rules! __concatMap {
     ($fn: expr, $target: expr) => {
         $target.into_iter()
@@ -568,18 +345,6 @@ macro_rules! __concatMap {
             .collect::<Vec<_>>()
     }
 }
-
-#[macro_export]
-macro_rules! __foldr {
-    ($fn: expr, $target: expr) => {
-        $target.into_iter()
-            .map($fn)
-            .collect::<Vec<_>>()
-    }
-}
-
-
-
 
 
 // IO fns
@@ -643,101 +408,14 @@ pub fn takeFileName(h: FilePath) -> FilePath {
     h
 }
 
-
-
-
 // TODO what do we do here:
 
 pub fn maybe<A, B, F: Fn(A) -> B>(default_: B, method: F, maybe: Option<A>) -> B {
     maybe.map(|x| method(x)).unwrap_or(default_)
 }
 
-pub fn bracket<A, B, C>(a: A, b: B, c: C) -> C {
-    // TODO these are all methods
-    c
-}
-
-
-pub fn seq<A, B>(a: A, b: B) -> B {
-    // we don't do lazy
-    b
-}
-
-
-
-
-
-
-// Map things
-
-pub mod Map {
-    pub fn insert<T>(mut okay: Vec<T>, key: isize, value: T) -> Vec<T> {
-        okay.push(value);
-        okay
-    }
-
-    pub fn lookup<T>(value: T, inside: Vec<T>) -> isize {
-        //TODO
-        0
-    }
-}
-
-
-use std::hash::Hash;
-use std::fmt::Debug;
-use std::collections::HashSet;
-
-#[derive(Clone, Debug)]
-pub struct Set<T: Eq + Hash>(HashSet<T>);
-
-impl<T: Eq + Hash + Debug> Set<T> {
-    pub fn member(item: T, list: Self) -> bool {
-        list.0.contains(&item)
-    }
-
-    pub fn fromList(list: Vec<T>) -> Self {
-        // TODO
-        Set(HashSet::new())
-    }
-
-    pub fn insert(item: T, mut list: Self) -> Self {
-        list.0.insert(item);
-        list
-    }
-
-    pub fn delete(item: T, mut list: Self) -> Self {
-        list.0.remove(&item);
-        list
-    }
-}
-
-
 // Array things
-
-pub fn array<T>(dim: (isize, isize), mut list: Vec<(isize, T)>) -> Vec<T> {
-    // Only supports an ordered array for now
-    list.sort_by(|a, b| a.0.cmp(&b.0));
-    assert_eq!(list.last().unwrap().0, dim.1 - 1);
-    list.into_iter().map(|x| x.1).collect()
-}
-
-pub fn listArray<T>(dim: (isize, isize), list: Vec<T>) -> Vec<T> {
-    list
-}
-
-pub struct Array<T, U> {
-    idx: Vec<T>,
-    inner: Vec<U>,
-}
 
 pub fn __op_array_index<T>(mut arr: Vec<T>, idx: isize) -> T {
     arr.remove(idx as usize)
 }
-
-pub fn __op_rshift(left: isize, right: isize) {
-    // TODO
-    unreachable!();
-}
-
-
-
