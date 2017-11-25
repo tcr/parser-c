cb1712d76974f5f3b9edc64afc667737c58f9e42
Georg Brandl <georg@python.org>
2017-11-25 16:05:46 +0100

    Work on InputStream APIs.

--- src/data/input_stream.rs
+++ src/data/input_stream.rs
@@ -2,6 +2,7 @@
 // File auto-generated using Corollary.
 
 use std::str;
+use std::char;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
@@ -11,8 +12,8 @@ use data::position::Position;
 #[derive(Debug)]
 pub struct InputStream {
     src: Vec<u8>,
-    rpos: usize,
-    tpos: usize,
+    peek_pos: usize,
+    tok_pos: usize,
 }
 
 impl InputStream {
@@ -20,34 +21,42 @@ impl InputStream {
     pub fn from_file<P: AsRef<Path>>(p: P) -> InputStream {
         let mut src = vec![];
         File::open(p.as_ref()).unwrap().read_to_end(&mut src).unwrap();
-        InputStream { src: src, rpos: 0, tpos: 0 }
+        InputStream { src: src, peek_pos: 0, tok_pos: 0 }
     }
 
     pub fn from_string(src: String) -> InputStream {
-        InputStream { src: src.into_bytes(), rpos: 0, tpos: 0 }
+        InputStream { src: src.into_bytes(), peek_pos: 0, tok_pos: 0 }
     }
 
     pub fn to_string(self) -> String {
-        String::from_utf8_lossy(&self.src[self.tpos..]).into_owned()
+        String::from_utf8_lossy(&self.src[self.tok_pos..]).into_owned()
     }
 
     pub fn is_done(&self) -> bool {
-        self.rpos == self.src.len()
+        self.peek_pos == self.src.len()
     }
 
-    pub fn read_byte(&mut self) -> u8 {
-        let byte = self.src[self.rpos];
-        self.rpos += 1;
-        byte
+    pub fn peek_byte(&mut self) -> Option<u8> {
+        if self.is_done() {
+            None
+        } else {
+            let byte = self.src[self.peek_pos];
+            self.peek_pos += 1;
+            Some(byte)
+        }
     }
 
-    pub fn last_char(&self) -> char {
-        // TODO correct unicode char handling
-        self.src[self.tpos] as char
+    pub fn last_char(&self) -> Option<char> {
+        char::decode_utf8(self.src[self.tok_pos..].iter().cloned()).next()?.ok()
     }
 
-    pub fn mark_read(&mut self, len: usize, pos: &mut Position) {
-        for &byte in &self.src[self.tpos..self.tpos+len] {
+    pub fn last_string(&self, len: usize) -> &str {
+        str::from_utf8(&self.src[self.tok_pos - len..self.tok_pos]).unwrap()
+    }
+
+    pub fn move_token(&mut self, len: usize, pos: &mut Position) {
+        let tok_str = &self.src[self.tok_pos..self.tok_pos+len];
+        for &byte in tok_str {
             match byte {
                 b'\n' => pos.inc_newline(),
                 // TODO: handle other control chars
@@ -58,11 +67,7 @@ impl InputStream {
                 _ => pos.inc_offset(1),
             }
         }
-        self.tpos += len;
-        self.rpos = self.tpos;
-    }
-
-    pub fn last_string(&self, len: usize) -> &str {
-        str::from_utf8(&self.src[self.tpos-len..self.tpos]).unwrap() // TODO UNICODE
+        self.tok_pos += len;
+        self.peek_pos = self.tok_pos;
     }
 }
